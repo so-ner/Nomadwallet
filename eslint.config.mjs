2f7736b6202a621f1916229f665f260ec30ee88c
ovokhj <ovokhj@gmail.com>
2025-10-11 13:31:55 +0900

    :bug: React JSX Scope 오류 및 빌드 실패 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,15 +3,35 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import {defineConfig} from "eslint/config";
+import nextConfig from "eslint-config-next";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     plugins: {js},
     extends: ["js/recommended"],
-    languageOptions: {globals: globals.browser},
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
     ignores: ['dist/*']
   },
   tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    ...pluginReact.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect" // 설치된 React 버전을 자동으로 감지
+      }
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+    }
+  },
+  ...nextConfig,
 ]);
