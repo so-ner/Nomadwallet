f57aa5f593edd6babe65e9d4a82742883abda40a
JIHEE JEONG <85853167+Insa14@users.noreply.github.com>
2025-10-11 19:26:19 +0900

    :zap: 여행 생성 및 삭제 API 구현

--- app/api/travel/[travel_id]/route.ts
+++ app/api/travel/[travel_id]/route.ts
@@ -25,14 +25,28 @@ export const PATCH = withAuth(async (user, req, contextPromise) => {
   const { params } = await contextPromise;
   const travelId = Number(params.travel_id);
 
+  // todo 인터페이스 생성 전 임시 타입 지정
   const body: { travel_title: string; total_budget: number } = await req.json()
   const { data, error } = await supabaseAdmin
     .from('travel')
-    .update({'travel_title' : body.travel_title, 'total_budget' : body.total_budget} )
+    .update({'travel_title' : body.travel_title, 'total_budget' : body.total_budget})
     .eq('travel_id', travelId)
 
   if (error) return NextResponse.json({ error: error.message }, { status: 400 })
   return NextResponse.json({ data })
 })
 
-// 삭제(DELETE, admin으로 진행할 것)
+// 삭제
+export const DELETE = withAuth(async (user, req, contextPromise) => {
+  const { params } = await contextPromise;
+  const travelId = Number(params.travel_id);
+
+  const { data, error } = await supabaseAdmin
+    .from('travel')
+    .delete()
+    .eq('user_id', user.id)
+    .eq('travel_id', travelId)
+
+  if (error) return NextResponse.json({ error: error.message }, { status: 400 })
+  return NextResponse.json({ data })
+})
