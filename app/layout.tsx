5d4c65f938e7d0461d752f25ba3035ca6d86fd28
ovokhj <ovokhj@gmail.com>
2025-11-17 18:10:21 +0900

    ✨ : 로그인 페이지 디자인시스템 적용 + 기능 개발 완료

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import {Geist, Geist_Mono} from 'next/font/google';
 import './globals.css';
 import {SessionProvider} from '@/providers/session';
 import Header from "@/component/Header";
+import {ToastProvider} from '@/context/ToastContext';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -23,12 +24,14 @@ export default function RootLayout({
     <html lang="ko">
     <body className={`${geistSans.variable} ${geistMono.variable}`}>
     <SessionProvider>
-      <div className="flex flex-col w-full min-h-screen md:justify-center md:items-center md:bg-page">
-        <article className="w-full flex-grow z-10 md:max-w-[600px] md:h-screen md:shadow-[0_0_20px_rgba(0,0,0,0.1)]">
-          <Header/>
-          {children}
-        </article>
-      </div>
+      <ToastProvider>
+        <div className="flex flex-col w-full min-h-screen md:justify-center md:items-center md:bg-page">
+          <article className="w-full flex-grow z-10 md:max-w-[600px] md:h-screen md:shadow-[0_0_20px_rgba(0,0,0,0.1)]">
+            <Header/>
+            {children}
+          </article>
+        </div>
+      </ToastProvider>
     </SessionProvider>
     </body>
     </html>
--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type {NextConfig} from "next";
 const nextConfig: NextConfig = {
   poweredByHeader: false, // 보안: X-Powered-By 제거
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'developers.google.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
